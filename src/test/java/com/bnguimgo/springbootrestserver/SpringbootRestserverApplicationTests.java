cc9e905dfa5fdfb7e53045d67af09f338b324601
Nassim Abed <nabed@sodifrance.fr>
2018-10-24 09:13:16 +0200

    init commit

---
+++ src/test/java/com/bnguimgo/springbootrestserver/SpringbootRestserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bnguimgo.springbootrestserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
